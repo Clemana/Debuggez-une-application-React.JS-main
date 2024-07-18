75c3f15f16b151e6aa4fa36e65335d187274af01
Clemana <ananaclem@gmail.com>
2024-07-18 22:05:54 +0200

    correction

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -5,30 +5,38 @@ import { getMonth } from "../../helpers/Date";
 import "./style.scss";
 
 const Slider = () => {
+
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+  
+    new Date(evtB.date) > new Date(evtA.date) ? 1 : -1
   );
+
   const nextCard = () => {
+    
+    if (byDateDesc) {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      
+      () => setIndex(index < byDateDesc.length -1 ? index + 1 : 0),
       5000
     );
+    }
   };
   useEffect(() => {
     nextCard();
   });
+
+  
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
+        
+         <div key={event.title} >
+         <div className={`SlideCard SlideCard--${
+                 index === idx ? "display" : "hide"
+             }`}
+         >
             <img src={event.cover} alt="forum" />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
@@ -37,20 +45,24 @@ const Slider = () => {
                 <div>{getMonth(new Date(event.date))}</div>
               </div>
             </div>
-          </div>
-          <div className="SlideCard__paginationContainer">
+         </div>
+         <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
+              {byDateDesc.map((e, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                
+                  key={`${e.title}`}
                   type="radio"
                   name="radio-button"
-                  checked={idx === radioIdx}
+                  
+                  checked={index === radioIdx}
+                  
+                  readOnly
                 />
               ))}
             </div>
-          </div>
-        </>
+         </div>
+        </div>
       ))}
     </div>
   );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,8 +13,13 @@ import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
-  const {last} = useData()
+  const { data } = useData();
   
+// const qui vient chercher la dernière prestation
+  const last = data && data.events && data.events.length > 0
+  ? [...data.events].sort((evtA, evtB) => new Date(evtB.date) - new Date(evtA.date))[0]
+  : null;
+
   return <>
     <header>
       <Menu />
@@ -23,7 +28,8 @@ const Page = () => {
       <section className="SliderContainer">
         <Slider />
       </section>
-      <section className="ServicesContainer">
+     
+      <section id="nos-services" className="ServicesContainer">
         <h2 className="Title">Nos services</h2>
         <p>Nous organisons des événements sur mesure partout dans le monde</p>
         <div className="ListContainer">
@@ -36,7 +42,7 @@ const Page = () => {
           </ServiceCard>
           <ServiceCard imageSrc="/images/hall-expo.png">
             <h3>Conférences</h3>
-            724 events vous propose d’organiser votre évènement, quelle que soit
+            77 events vous propose d’organiser votre évènement, quelle que soit
             sa taille, en s’adaptant à votre demande et à vos demandes. En tant
             que spécialistes de l’évènementiel, nous saurons trouver le lieu
             parfait ainsi que des solutions inédites pour capter votre audience
@@ -52,11 +58,13 @@ const Page = () => {
           </ServiceCard>
         </div>
       </section>
-      <section className="EventsContainer">
+     
+      <section id="nos-realisations" className="EventsContainer">
         <h2 className="Title">Nos réalisations</h2>
         <EventList />
       </section>
-      <section className="PeoplesContainer">
+      
+      <section id="notre-equipe" className="PeoplesContainer">
         <h2 className="Title">Notre équipe</h2>
         <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
         <div className="ListContainer">
@@ -117,19 +125,24 @@ const Page = () => {
     <footer className="row">
       <div className="col presta">
         <h3>Notre derniére prestation</h3>
-        <EventCard
-          imageSrc={last?.cover}
-          title={last?.title}
-          date={new Date(last?.date)}
-          small
-          label="boom"
-        />
+        
+         {last && (
+            <EventCard
+              data-testid="last-event-card"
+              imageSrc={last?.cover}
+              imageAlt={last?.description}
+              title={last?.title}
+              date={new Date(last?.date)}
+              small
+              label={last?.type}
+            />
+          )}
       </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
         <address>45 avenue de la République, 75000 Paris</address>
         <div>01 23 45 67 89</div>
-        <div>contact@724events.com</div>
+        <div>contact@77events.com</div>
         <div>
           <a href="#twitch">
             <Icon name="twitch" />
